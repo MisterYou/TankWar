5c859a6cd951b9a492f9b85c9803a3807af646e4
MisterYou <1206919076@qq.com>
2015-11-09 15:54:59 +0800

    坦克大战3.0

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,10 +1,19 @@
 import java.awt.*;
+import java.awt.event.*;
 
 public class TankClient extends Frame{
 	
 	public void lunchFrame(){
 		this.setLocation(400,300);
 		this.setSize(800,600);
+		this.setTitle("TankWar");
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+			
+		});//关闭窗口，事件监听
+		this.setResizable(false);//禁止改变大小
 		setVisible(true);
 	}
 
